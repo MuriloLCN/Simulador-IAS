cb89c0a6cd1634d535f02d25695a4baec26529c7
Murilo Luis Calvo Neves <muriloluiscalvoneves2004@hotmail.com>
2023-12-12 12:33:35 -0300

    Uso do startsWith para ver prefixo de operações

--- ias.c
+++ ias.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {False, True} booleano;
+
 void OffSpace (char line[])
 {
     for (int i = 0; i < strlen(line); i++)
@@ -46,21 +48,170 @@ int GetOperand (char instruction[], char operand[])
     return 0;
 }
 
+/*
+Instruções - Opcode
+LOAD MQ - 00001010
+LOAD MQ,M(X) - 00001001
+STOR M(X) - 00100001
+LOAD M(X) - 00000001
+LOAD -M(X) - 00000010
+LOAD |M(X)| - 00000011
+LOAD -|M(X)| - 00000100
+
+JUMP M(X,0:19) - 00001101
+JUMP M(X,20:39) - 00001110
+
+JUMP +M(X,0:19) - 00001111
+JUMP +M(X,20:39) - 00010000
+
+ADD M(X) - 00000101
+ADD |M(X)| - 00000111
+SUB M(X) - 00000110
+SUB |M(X)| - 00001000
+MUL M(X) - 00001011
+DIV M(X) - 00001100
+LSH - 00010100
+RSH - 00010101
+STOR M(X,8:19) - 00010010
+STOR M(X,28:39) - 00010011
+
+EXIT - 11111111
+*/
+
+booleano startsWith(char* palavra, char* prefixo)
+{
+    /*
+        Verifica se uma palavra começa com algum prefixo específico
+        Parâmetros:
+            char* palavra: A string a qual se quer ver o prefixo
+            char* prefixo: O prefixo a ser comparado com o início da palavra
+        Retorno:
+            True: Caso a palavra tenha o prefixo
+            False: Caso a palavra não tenha o prefixo
+    */
+    int tamanho_palavra = strlen(palavra);
+    int tamanho_prefixo = strlen(prefixo);
+
+    // printf("\nComparando %s com prefixo %s...", palavra, prefixo);
+
+    if (tamanho_prefixo > tamanho_palavra)
+    {
+        return False;
+    }
+
+    int i = 0;
+    while (i < tamanho_prefixo)
+    {
+        if (palavra[i] != prefixo[i])
+        {
+            return False;
+        }
+        i++;
+    }
+
+    // printf(": True");
+    return True;
+}
+
+booleano endsWith(char* palavra, char* sufixo)
+{
+    /*
+        Verifica se uma palavra termina com algum sufixo específico
+        Parâmetros:
+            char* palavra: A string a qual se quer ver o prefixo
+            char* sufixo: O sufixo a ser comparado com o fim da palavra
+        Retorno:
+            True: Caso a palavra tenha o sufixo
+            False: Caso a palavra não tenha o sufixo
+    */
+    int tamanho_palavra = strlen(palavra);
+    int tamanho_sufixo = strlen(sufixo);
+
+    if (tamanho_sufixo > tamanho_palavra)
+    {
+        return False;
+    }
+
+    int i = tamanho_palavra - 1;
+    int j = tamanho_sufixo - 1;
+    while (j >= 0)
+    {
+        if (palavra[i] != sufixo[j])
+        {
+            return False;
+        }
+        i--;
+        j--;
+    }
+    return True;
+}
+
 int main ()
 {
     char instruction[100], opcode[6], operand[6];
     FILE *f;
     f = fopen("instructions.txt", "r");
+    int linha = 0;
     while(fgets(instruction, 100, f) != NULL)
     {
-        GetOpCode(instruction, opcode);
-        printf("Opcode: %s\n", opcode);
+        linha++;
+        // GetOpCode(instruction, opcode);
+        // // printf("Opcode: %s\n", opcode);
 
-        GetOperand(instruction, operand);
-        printf("Operand: %s\n", operand);
+        // GetOperand(instruction, operand);
+        // printf("Operand: %s\n", operand);
 
-        if (strcmp(instruction, "LOAD") == 0)  printf("000000001\n");
-        else if (strcmp(instruction, "ADD") == 0) printf("000000010\n");
+        // printf("\n %d: %s", linha, instruction);
+        printf("\n");
+
+        if (startsWith(instruction,"LOAD"))
+        {
+            // Todos os casos de load entram aqui...
+            printf("> linha %d era um LOAD", linha);
+        }
+        else if (startsWith(instruction,"STOR"))
+        {
+            // Todos os casos de stor entram aqui...
+            printf("> linha %d era um STOR", linha);
+        }
+        else if (startsWith(instruction,"JUMP"))
+        {
+            printf("> linha %d era um JUMP", linha);
+        }
+        else if (startsWith(instruction,"ADD"))
+        {
+            printf("> linha %d era um ADD", linha);
+        }
+        else if (startsWith(instruction,"SUB"))
+        {
+            printf("> linha %d era um SUB", linha);
+        }
+        else if (startsWith(instruction,"MUL"))
+        {
+            printf("> linha %d era um MUL", linha);
+        }
+        else if (startsWith(instruction,"DIV"))
+        {
+            printf("> linha %d era um DIV", linha);
+        }
+        else if (startsWith(instruction,"LSH"))
+        {
+            printf("> linha %d era um LSH", linha);
+        }
+        else if (startsWith(instruction,"RSH"))
+        {
+            printf("> linha %d era um RSH", linha);
+        }
+        else if (startsWith(instruction,"EXIT"))
+        {
+            printf("> linha %d era um EXIT", linha);
+        }
+        else
+        {
+            printf("\n> Erro: Comando '%s' nao reconhecido, linha %d", opcode, linha);
+        }
+        // if (strcmp(instruction, "LOAD") == 0)  printf("000000001\n");
+        // else if (strcmp(instruction, "ADD") == 0) printf("000000010\n");
     }
 
     fclose(f);
